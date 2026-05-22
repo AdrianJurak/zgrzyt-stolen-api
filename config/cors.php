14cd4aafaaa9fd15229324277800bc7147d45b15
AdrianJurak <adrian.jurak@studenci.collegiumwitelona.pl>
2026-05-23 01:12:36 +0200

    Fix: chages for web deploy

--- config/cors.php
+++ config/cors.php
@@ -23,6 +23,7 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://192.168.100.8:5173',
+        'https://zgrzyt-web.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
