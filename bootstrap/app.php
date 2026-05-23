2dcb275fc4896ddad368aaa58ce4f58cdc8231b6
AdrianJurak <adrian.jurak@studenci.collegiumwitelona.pl>
2026-05-23 02:34:41 +0200

    Fix: and some more for good measure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,9 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\Http\Middleware\HandleCors::class,
-        ]);
-
-        $middleware->trustHosts(at: ['zgrzyt-9voev8far-adrianj-s-projects.vercel.app', 'zgrzyt-stolen-api.onrender.com']);
+        ]);;
 
         $middleware->validateCsrfTokens(except: [
             '/logout',
