a8eb766ab10a0c957bcd4459e2949cf079937dd1
AdrianJurak <adrian.jurak@studenci.collegiumwitelona.pl>
2026-05-23 02:02:04 +0200

    Fix: some more changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
+        $middleware->statefulApi();
+
         $middleware->api(prepend: [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Http\Middleware\HandleCors::class,
         ]);
 
